38ef42d29fa2f5b0fad4bf11c6787f0bb118af03
Christopher N. Hesse <raymanfx@gmail.com>
2016-11-19 16:46:08 +0100

    sepolicy: Require targets to explicitly opt out
    Change-Id: Ibcfec01af69f41ee20d4242e2183292ed7d7818f

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 # Board specific SELinux policy variable definitions
+ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
 BOARD_SEPOLICY_DIRS := \
